d7e99ba6ae70221616e145badadfa4b24270e7a0
William Hughes <will@willhughes.name>
2024-05-15 21:05:03 +1200

    v0.1.0

--- App/Restic/common.sh
+++ App/Restic/common.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION="0.0.0"
+VERSION="0.1.0"
 STARTUP_SCRIPT=/mnt/SDCARD/.tmp_update/startup/restic_autostart.sh
 DATA_DIR=/mnt/SDCARD/.restic
 LAST_BACKUP_FILE="${DATA_DIR}/restic_last_backup"
